51cab4d62da62b1edc0957faba54f7a53e88a0d4
AniMuoz <anibalmunoz74@gmail.com>
2024-08-28 22:34:35 -0400

    cambios finales del dia, añadido personal y medio añadido pago

--- modulo2/src/app/app.component.ts
+++ modulo2/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ListaProductosComponent } from "./lista-productos/lista-productos.component";
 import { NavbarComponent } from "./navbar/navbar.component";
+import { PersonalComponent } from "./personal/personal.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ListaProductosComponent, NavbarComponent],
+  imports: [RouterOutlet, ListaProductosComponent, NavbarComponent, PersonalComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
