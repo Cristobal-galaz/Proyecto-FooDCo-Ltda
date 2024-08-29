0e6a85f66b42265e95f23add74a609b65e942b1a
AniMuoz <anibalmunoz74@gmail.com>
2024-08-28 20:08:29 -0400

    se creo componente

---
+++ modulo2/src/app/lista-productos/lista-productos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-productos',
+  standalone: true,
+  imports: [],
+  templateUrl: './lista-productos.component.html',
+  styleUrl: './lista-productos.component.css'
+})
+export class ListaProductosComponent {
+
+}
