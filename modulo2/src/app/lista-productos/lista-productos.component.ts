a63334691e4aee3ce587130fa1c7c4db64d8ea14
AniMuoz <anibalmunoz74@gmail.com>
2024-08-28 20:53:53 -0400

    test de productos

--- modulo2/src/app/lista-productos/lista-productos.component.ts
+++ modulo2/src/app/lista-productos/lista-productos.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-lista-productos',
@@ -8,5 +10,32 @@ import { Component } from '@angular/core';
   styleUrl: './lista-productos.component.css'
 })
 export class ListaProductosComponent {
+  isChatOpen = false;
+
+  campaigns = [
+    {
+      id: 1,
+      name: 'Campaña 1',
+      type: 'Radial',
+      duration: 'HIGH',
+      price: 700,
+      date: '2024-08-14',
+      managerImg: 'https://yt3.googleusercontent.com/...'
+    },
+    // Agrega más campañas aquí
+  ];
+
+  toggleChat() {
+    this.isChatOpen = !this.isChatOpen;
+  }
+
+  sendMessage(event: KeyboardEvent) {
+    const input = (event.target as HTMLInputElement);
+    const message = input.value.trim();
+    if (event.key === 'Enter' && message) {
+      // Lógica para enviar el mensaje
+      input.value = '';
+    }
+  }
 
 }
