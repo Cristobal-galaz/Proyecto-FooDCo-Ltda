72507b04b7d4b60ba05e21c68de873feccbf0e46
AniMuoz <anibalmunoz74@gmail.com>
2024-09-01 22:53:45 -0400

    Inicio hecho

--- modulo2/src/app/app.component.ts
+++ modulo2/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { ListaProductosComponent } from "./lista-productos/lista-productos.compo
 import { NavbarComponent } from "./navbar/navbar.component";
 import { PersonalComponent } from "./personal/personal.component";
 import { CarritoComponent } from './carrito/carrito.component';
+import { InicioComponent } from "./inicio/inicio.component";
 
 
 
@@ -11,7 +12,7 @@ import { CarritoComponent } from './carrito/carrito.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ListaProductosComponent, NavbarComponent, PersonalComponent, CarritoComponent,],
+  imports: [RouterOutlet, ListaProductosComponent, NavbarComponent, PersonalComponent, CarritoComponent, InicioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
