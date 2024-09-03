6a7c720f1eefa0b27a6b7bfee98036ff937e187d
acorreap <d.vergarabustamante@uandresbello.edu>
2024-09-03 12:26:31 -0400

    se elimino lo no que no sirve
    hicimos puras weas

--- modulo2/src/app/app.component.ts
+++ modulo2/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ListaProductosComponent } from "./lista-productos/lista-productos.component";
+
 import { NavbarComponent } from "./navbar/navbar.component";
 import { PersonalComponent } from "./personal/personal.component";
-import { CarritoComponent } from './carrito/carrito.component';
+
 import { InicioComponent } from "./inicio/inicio.component";
 
 
@@ -12,7 +12,7 @@ import { InicioComponent } from "./inicio/inicio.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ListaProductosComponent, NavbarComponent, PersonalComponent, CarritoComponent, InicioComponent],
+  imports: [RouterOutlet, NavbarComponent, PersonalComponent, InicioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
