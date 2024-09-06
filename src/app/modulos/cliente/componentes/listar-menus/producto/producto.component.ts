a2487fd1d648cc9625405d2d170fc567d65f5bf5
acorreap <a.correapea@uandresbello.edu>
2024-09-06 15:23:55 +0000

    arreglo de tarjetas 06/09/2024

--- src/app/modulos/cliente/componentes/listar-menus/producto/producto.component.ts
+++ src/app/modulos/cliente/componentes/listar-menus/producto/producto.component.ts
@@ -3,11 +3,15 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import { Producto, CantidadProducto } from '../../../interfaces/alimento';
 import { ListaProductosService } from '../../../services/lista-productos.service';
+import {MatIconModule} from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-producto',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, FormsModule, MatInputModule, MatFormFieldModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './producto.component.html',
   styleUrl: './producto.component.scss'
@@ -18,6 +22,12 @@ constructor(private listaProductosService: ListaProductosService,
 ){}
 
   @Input() cardProduct!: Producto;
+  cantidadProducto: number = 1;
+  showInput = false;
+
+  onSelect(){
+    this.showInput = true;
+  }
 
   onClick(producto: Producto) {
     console.log('Producto seleccionado', producto._id);
