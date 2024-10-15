55748514be6eac4e03bf1acc4082e5640dc01e0d
dvergara123 <d.vergarabustamante@uandresbello.edu>
2024-10-15 11:57:54 -0300

    ingles en orden de compra api

--- src/app/modulos/ventas/componentes/English-comp/orden-compra-en/orden-compra-en.component.ts
+++ src/app/modulos/ventas/componentes/English-comp/orden-compra-en/orden-compra-en.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ApiserviceService } from '../../../Service/apiservice.service';
+import { OrdenCompra } from '../../../interface/ordendecompra';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-orden-compra-en',
@@ -8,10 +11,35 @@ import { Router } from '@angular/router';
   styleUrls: ['./orden-compra-en.component.css']
 })
 export class OrdenCompraEnComponent {
-  constructor(private router: Router) {}
+  orden!: OrdenCompra;
+  constructor(private router: Router, private ordenCompra: ApiserviceService) {}
 
   switchToSpanish() {
     // Redirige a la versión en español de la orden de compra
     this.router.navigate(['/orden-compra']);
+  }
+  ngOnInit(): void {
+    this.ordenCompra.getOrdenComprajefedeventa().pipe(
+      map((data: any) => {
+        return {
+          _id: data._id,
+          cliente: data.cliente,
+          estado: data.estado,
+          seleccionProductos: data.seleccionProductos,
+          precioTotalOrden:data.precioTotalOrden,
+          iva:data.iva,
+          precioFinalConIva:data.precioFinalConIva,
+        } as OrdenCompra;
+      })
+    ).subscribe(
+      (ordenCompra: OrdenCompra) => {
+        this.orden = ordenCompra;
+        console.log(this.orden);
+        console.log(this.orden.seleccionProductos.productos[0].descuento);
+      },
+      (error) => {
+        console.error('Error al cargar la orden de compra', error);
+      }
+    );
   }
 }
