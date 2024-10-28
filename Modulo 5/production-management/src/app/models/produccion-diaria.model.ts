6f2ad7fd1ec704ceb48806e7cbe845ccd75d38df
c66dx <c.barraotto@uandresbello.edu>
2024-10-28 19:05:21 -0300

    .

--- Modulo 5/production-management/src/app/models/produccion-diaria.model.ts
+++
@@ -1,7 +0,0 @@
-export interface ProduccionDiaria {
-    id: number;
-    tipo_producto_id: number;
-    cantidad_producida: number;
-    fecha_produccion: Date;
-}
-  
--- Modulo 5/production-management/src/app/models/tipo-producto.model.ts
+++
@@ -1,4 +0,0 @@
-export interface TipoProducto {
-  id: string;
-  nombre: string;
-}
