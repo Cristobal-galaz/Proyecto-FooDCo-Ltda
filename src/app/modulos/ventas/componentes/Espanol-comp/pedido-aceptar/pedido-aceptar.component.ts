ca9f84432776094ce066c08f85bfcf4debe17743
AniMuoz <anibalmunoz74@gmail.com>
2024-11-19 21:11:12 -0300

    placeholder de pedido, falta unir a api

--- src/app/modulos/ventas/componentes/Espanol-comp/pedido-aceptar/pedido-aceptar.component.ts
+++ src/app/modulos/ventas/componentes/Espanol-comp/pedido-aceptar/pedido-aceptar.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pedido-aceptar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pedido-aceptar.component.html',
   styleUrl: './pedido-aceptar.component.scss'
 })
 export class PedidoAceptarComponent {
+  data = [
+    { name: 'Pedido 1' },
+    { name: 'Pedido 2' },
+    { name: 'Pedido 3' }
+  ];
 
+  accept(item: any): void {
+    console.log('Aceptado:', item);
+    this.data = this.data.filter(d => d !== item);
+  }
+
+  reject(item: any): void {
+    console.log('Rechazado:', item);
+    this.data = this.data.filter(d => d !== item);
+  }
 }
